43b94a57caaa6d232569f4720293e2979bfaa4a6
FBlanco <fblanco@MPFBlanco.local>
2017-08-15 22:48:27 -0500

    react request to api rest with express

--- server.js
+++ server.js
@@ -3,6 +3,23 @@ const app = express();
 
 app.use(express.static(__dirname + '/public'));
 
+const users = [
+    {
+        nombre: "fabian"
+    },
+    {
+        nombre: "Pedro"
+    }
+]
+
+app.get('/api/users', (req, res) => {
+    res.json({
+        users: users
+    })
+});
+
+
+
 app.listen(3000, () => {
     console.log('server on port 3000');
 });
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,36 @@
 import React, {Component} from 'react';
+import request from 'superagent';
+
 
 export default class App extends Component{
+    constructor(){
+        super();
+        this.state = {
+            users: []
+        }
+    }
+    componentDidMount(){
+        request
+            .get('http://localhost:3000/api/users/')
+            .end((err, res) => {
+                console.log(JSON.parse(res.text))
+                const users = JSON.parse(res.text).users;
+                this.setState({
+                    users: users
+                })
+            });
+    }
     render(){
+        var users = this.state.users.map((user, i) => {
+            return <li key={i}>{user.nombre}</li>
+        });
         return(
-            <h1>My app updated 3.4</h1>
+            <div>
+                <h1>Consumiendo API express con React</h1>
+                <ul>
+                    {users}
+                </ul>
+            </div>
         )
     }
 }
